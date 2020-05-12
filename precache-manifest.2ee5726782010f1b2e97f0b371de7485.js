4a6f52482f41c8720588a3f260f469407b2f07a3
Ferencz Laszlo Botond <laszlo@ibee.ro>
2020-05-12 22:12:02 +0300

    Updates

---
+++ precache-manifest.2ee5726782010f1b2e97f0b371de7485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a55f41459b5de01b8d50050862ffa3",
+    "url": "/qa/index.html"
+  },
+  {
+    "revision": "7eb936ec3a23e2185883",
+    "url": "/qa/static/css/main.3bb34a30.chunk.css"
+  },
+  {
+    "revision": "343bbff98d98341dec82",
+    "url": "/qa/static/js/2.0b528e3a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/qa/static/js/2.0b528e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb936ec3a23e2185883",
+    "url": "/qa/static/js/main.8788439f.chunk.js"
+  },
+  {
+    "revision": "c280d99d3dc7b6204496",
+    "url": "/qa/static/js/runtime-main.28dd9671.js"
+  }
+]);
